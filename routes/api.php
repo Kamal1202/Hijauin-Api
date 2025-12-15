242f20cd86060399c9289c5a528bd1717f660267
Kamal1202 <suryakamal1202@gmail.com>
2025-12-15 10:05:11 +0700

    Ada ubah di api.php sama nambah setorancontroller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\SetoranController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -13,6 +14,7 @@ Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']); //
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']); // Untuk logout
 Route::middleware('auth:sanctum')->put('/profile', [AuthController::class, 'updateProfile']); // Untuk update profile
 Route::middleware('auth:sanctum')->get('/dashboard', [DashboardController::class, 'index']); // Ini buat Dashboard
+Route::middleware('auth:sanctum')->get('/setoran', [SetoranController::class, 'index']); // Dari setorancontroller buat history setoran 
 
 //ambil data artikel
 Route::get('/articles', [ArticleController::class, 'index']);
